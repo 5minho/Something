d64c689b68ed45b2d42973c7dfd180e4f035b5df
minomi <5minhominho@gmail.com>
2019-10-14 11:52:48 +0900

    create ping controller

---
+++ src/main/java/me/minho/something/SomethingApplication.java
@@ -0,0 +1,14 @@
+package me.minho.something;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SomethingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomethingApplication.class, args);
+    }
+
+}
